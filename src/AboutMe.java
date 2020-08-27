b4ad693918d548df1ac1dd4ecf12bb5190f909bc
Robert Ardeleanu <robert.ardeleanu@gmx.at>
2020-08-27 12:13:37 +0200

    Use printf instead

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -2,16 +2,15 @@ public class AboutMe {
 
     public static void main(String[] args) {
 
-        String firstName = " Robert";
-        String lastName = " Ardeleanu";
-        String birthday = " 6th July";
-        String[] hobbies = {" drawing,", " screenprinting,", " handcrafting,", " web developement"};
+        String firstName = "Robert";
+        String lastName = "Ardeleanu";
+        String birthday = "6th July";
+        String[] hobbies = {"drawing,", "screenprinting,", "handcrafting", "web developement"};
         char gender = 'm';
         boolean isMarried = false;
         int averageGrade = 5;
 
-        System.out.println("My name is" + firstName + lastName + ". My birthday is on" + birthday + " and my gender is " + gender + ". " + "My hobbies are " + hobbies[0] + hobbies[1] + hobbies[2] + hobbies[3] + "." + " Am I married?: " + isMarried + "." + " My average grade is " + averageGrade + ".");
-
+        System.out.printf("%s %s %s. %s %s %s %s. %s %s %s %s %s %s. %s %s. %s %d. ", "My name is", firstName, lastName, "My Birthday is on", birthday, "and my gender is", gender, "My hobbies are", hobbies[0], hobbies[1], hobbies[2], "and", hobbies[3], "Am i married?:", isMarried, "My average grade is", averageGrade);
     }
 
 }
